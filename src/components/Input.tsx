8c9cfac0d7747971797009fc29be72a0803f5ad9
Ting <tchangysl@gmail.com>
2024-03-12 12:30:31 +0800

    refactor: fix ts errors

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,11 +3,11 @@ import { Input as BaseInput, InputProps } from '@mui/base/Input';
 const INPUT_STYLE =
   'border-2 border-greyWhite focus:outline-none focus:border-tutorOrange bg-transparent rounded-md p-4 text-sm';
 
-export default function Input(props: InputProps) {
+export default function Input({ onChange, placeholder }: InputProps) {
   return (
     <BaseInput
       slotProps={{
-        input: { className: INPUT_STYLE, onChange: props.onChange },
+        input: { className: INPUT_STYLE, onChange, placeholder },
       }}
     />
   );
--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,13 +1,13 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, ChangeEventHandler } from 'react';
 
 export default function useInput(initialValue: string) {
   const [value, setValue] = useState(initialValue);
 
-  const changeValue = (e) => {
+  const changeValue: ChangeEventHandler<HTMLInputElement> = (e) => {
     setValue(e.target.value);
   };
 
-  return [value, changeValue];
+  return [value, changeValue] as const;
 }
